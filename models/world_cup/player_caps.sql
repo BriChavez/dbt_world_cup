1f7b544deef3b045c8dbb44d23f962d4104fa6b5
brianachavez@gmail.com <brianachavez@gmail.com>
2022-07-18 00:58:26 +0000

    I did it! i got a ref function to work

--- models/world_cup/player_caps.sql
+++ models/world_cup/player_caps.sql
@@ -1,7 +1,11 @@
--- with Player_Caps as (
---   select Player,
---         Position,
---         Caps,
---   from {{ ref('players') }}
---   order by Caps desc
--- )
+
+select 
+    Player,
+    Position,
+    Caps
+from 
+    {{ ref('players') }}
+where 
+    Caps is not null
+order by 
+    Caps desc
