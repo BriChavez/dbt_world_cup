719848671e84cb8493f5010d2fcf32ee411dd691
brianachavez@gmail.com <brianachavez@gmail.com>
2022-07-18 06:19:26 +0000

    players caps finally in

--- models/world_cup/player_caps.sql
+++ models/world_cup/player_caps.sql
@@ -1,11 +1,12 @@
-
 select 
     Player,
     Position,
-    Caps
+    Caps,
+    Club
 from 
     {{ ref('players') }}
 where 
-    Caps is not null
+    Caps not like '%0%' and Caps not like 'n/a'
 order by 
     Caps desc
+    
