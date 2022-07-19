79f65068486ff71675160a4cec71b7693d3eea2e
brianachavez@gmail.com <brianachavez@gmail.com>
2022-07-19 07:31:01 +0000

    save before pulling'

--- models/world_cup/player_caps.sql
+++ models/world_cup/player_caps.sql
@@ -1,12 +1,16 @@
-select 
-    Player,
-    Position,
-    Caps,
-    Club
-from 
-    {{ ref('players') }}
-where 
-    Caps not like '%0%' and Caps not like 'n/a'
-order by 
-    Caps desc
-    
+with player_caps as (
+    select 
+        Player,
+        Position,
+        Caps,
+        Club
+    from 
+        {{ ref('players') }}
+    where 
+        Caps not like '%0%' and Caps not like 'n/a'
+    order by 
+        Caps desc
+)
+
+
+select * from player_caps
